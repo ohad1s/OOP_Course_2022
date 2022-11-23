13c2fe8d7886c426e16e473de2c2fc9b17cc22cf
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-11-23 19:03:46 +0200

    TA3

---
+++ TA3/tirgul3.py
@@ -0,0 +1,75 @@
+import random as rand
+import math as mt
+mt.sqrt(25)
+print("hello world")
+if 5>2:
+    if 5>3:
+        if 5>4:
+            print("dsfds")
+x=5
+y="5"
+bool= True
+bool2= False
+# skjfshdkfhkadf
+'''
+cobsjkfbdskfsfhkjsflks
+'''
+print(type(x))
+y=int(y)
+w = t =u =10
+x ,y ,z = 1,2,3
+mylist=[1,2,3,"fsifh",True,x,y,5.0]
+mylist.append("dsfbdsjkfd")
+mylist2= [1,2,3]
+x,y,z= mylist2
+print(5*"a")
+mystr= "ohad shirazi"
+print(mystr[0])
+print(mystr[0:12:3])
+print(mystr[-1])
+print(mystr[-1:-12:])
+len(mystr)
+# x=input("enter a number")
+# y=input("enter a number")
+print("ohad.y,.,dsfds.fdsfs".split("."))
+# if x>y:
+#     print("yes")
+# elif x==y:
+#     print("cii")
+# else:
+#     print("nooo")
+# print("o" in mystr)
+# for i in range(1,10,2):
+#     print(i)
+# for char in mystr:
+#     print(char)
+# for element in mylist2:
+#     print(element)
+mylist3= ["a", "b", "c"]
+for i,elem  in enumerate(mylist3):
+    print(elem)
+    print(i)
+
+mytup=(1,2,3,8)
+mystr[0]="s"
+x=10
+def myfunc(z:int,y:str):
+    global x
+    print()
+    return x, y,z
+
+def isPalindrome(mystr):
+    start = 0
+    end= len(mystr)
+    while start<end:
+        if mystr[start]!=mystr[end]:
+            return False
+        start+=1
+        end-=1
+    else:
+        return True
+
+
+
+
+
---
+++ TA3/tirgul3_2.py
@@ -0,0 +1,73 @@
+# import math
+# math.sqrt(6)
+# print("Hello World")
+# booli= True
+# x= 5
+# y= 7.7
+# x= "ohad"
+# zzz= "5"
+# print(type(zzz))
+# zzz=int(zzz)
+# print(type(zzz))
+# # this is com...
+# """
+# hkdsglshf
+# gdfjkhuke
+# gerjkghlkrqg
+# ljgbherjkghker
+# """
+# x, y,z = 5, 6, 7
+# x = y =z =9
+# mystr= "ohad shirazi"
+# print(mystr[0])
+# print(mystr[-1])
+# print(mystr[0:9:2])
+# print("hello"+mystr)
+# # mystr+5
+# print(5*"a")
+# mylist2=[1,2, y]
+# mylist =[True, "nlkjghsl", 5 , x, mystr,mylist2,[1,2,3]]
+# print(mylist)
+# mylist.append(7)
+# mylist.append(12)
+# mylist.extend([1,2,3])
+# x ,y ,z = mylist2
+# len(mylist)
+# print("ohad.yossi.trtr.trtr.zzzz".split("t"))
+# first = int(input("enter a num"))
+# second= int(input("enter a num2"))
+# if first>second:
+#     print("yes")
+#     print("ciiii")
+#     if 7>first:
+#         print("kfherfkgrwg")
+# elif first==second:
+#     print("======")
+# else:
+#     print("khfgadshfgask")
+
+# mystr= "ohad shirazi"
+# mylist=[777,257834,mystr,True]
+# for index, element in enumerate(mylist):
+#     print(index)
+#     print(element)
+mytup=(7,"fjskfh",True)
+x=10
+i=5
+while x>i:
+    print(x)
+    i+=1
+else:
+    print("end")
+
+
+def myfunc(x,y):
+    z=10
+    print(x)
+    return y+x, z
+
+q ,t = myfunc(10,20)
+
+
+
+
