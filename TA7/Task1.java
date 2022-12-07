594ce0a14d7be52b482a8278487929bc5e772712
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-12-07 20:18:15 +0200

    task1

--- TA7/Task1.java
+++ TA7/Task1.java
@@ -1,5 +1,50 @@
 package TA7;
 
+class myFile {
+    private String path;
+    private String password;
+
+    public myFile(String path, String password) {
+        this.path = path;
+        this.password = password;
+    }
+
+}
+
 public class Task1 {
-//    solve the fileLockingProblemDeadlock
+
+    //    solve the fileLockingProblemDeadlock
+    public static void main(String[] args) {
+        myFile f = new myFile("path", "pasword");
+
+        Thread t1 = new Thread() {
+            public void run() {
+                System.out.println(Thread.currentThread().getName()+" trying open the file..");
+                synchronized (f) {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (Exception e) {
+                    }
+                    System.out.println(Thread.currentThread().getName()+" I Have the password and i can open the file !!!");
+                }
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                System.out.println(Thread.currentThread().getName()+" trying open the file..");
+                synchronized (f) {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (Exception e) {
+                    }
+                    System.out.println(Thread.currentThread().getName()+ " I Have the password and i can open the file !!!");
+                }
+            }
+        };
+        t1.setName("t1");
+        t2.setName("t2");
+        t1.start();
+        t2.start();
+    }
 }
